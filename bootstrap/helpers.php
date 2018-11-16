68361eaded5f04879020363202f3d532e71bf8f9
Darren-Lin <554515127@qq.com>
2018-11-16 22:56:00 +0800

    基础布局

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -4,3 +4,8 @@
  * Email:  linderun@linderun.com
  * Since:  2018/11/16 22:27
  */
+
+function route_class()
+{
+    return str_replace('.', '-', Route::currentRouteName());
+}
---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由 <a href="http://www.linderun.com" target="_blank">Darren Lin</a> 设计和编码 <span
+                    style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="pull-right"><a href="mailto:linderun@linderun.com">联系我们</a></p>
+    </div>
+</footer>
