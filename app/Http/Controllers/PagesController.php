418b8e595c24de4f3a63d75b9c5dfbead8397ba5
Darren-Lin <554515127@qq.com>
2018-11-18 16:14:16 +0800

    邮箱验证中间件

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
     public function root()
     {
         return view('pages.root');
+    }
+
+    public function emailVerifyNotice()
+    {
+        return view('pages.email_verify_notice');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,14 @@
 Route::get('/', 'PagesController@root')->name('root');
 
 Auth::routes();
+
+
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
+
+    Route::group(['middleware' => 'email_verified'], function() {
+        Route::get('/test', function() {
+            return 'Your email is verified';
+        });
+    });
+});
