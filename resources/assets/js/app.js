81f1e08e1ba1228c4744e97656af6ea31ec576cc
Darren-Lin <554515127@qq.com>
2018-11-18 18:01:28 +0800

    新增收货地址

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -15,7 +15,8 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+require('./components/SelectDistrict');
+require('./components/UserAddressesCreateAndEdit');
 
 const app = new Vue({
     el: '#app'
--- routes/web.php
+++ routes/web.php
@@ -23,5 +23,7 @@ Route::group(['middleware' => 'auth'], function (){
 
     Route::group(['middleware' => 'email_verified'], function (){
         Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+        Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');
+        Route::post('user_addresses', 'UserAddressesController@store')->name('user_addresses.store');
     });
 });
