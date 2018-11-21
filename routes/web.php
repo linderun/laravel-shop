46169c3a1272ccdb9442dab9105d5732176a8ab4
Darren-Lin <554515127@qq.com>
2018-11-21 22:55:11 +0800

    用户界面 - 商品列表

--- routes/web.php
+++ routes/web.php
@@ -11,17 +11,19 @@
 |
 */
 
-Route::get('/', 'PagesController@root')->name('root');
+Route::redirect('/', '/products')->name('root');
+
+Route::get('products', 'ProductsController@index')->name('products.index');
 
 Auth::routes();
 
 
-Route::group(['middleware' => 'auth'], function (){
+Route::group(['middleware' => 'auth'], function () {
     Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
     Route::get('/email_verification/send', 'EmailVerificationController@send')->name('email_verification.send');
     Route::get('/email_verification/verify', 'EmailVerificationController@verify')->name('email_verification.verify');
 
-    Route::group(['middleware' => 'email_verified'], function (){
+    Route::group(['middleware' => 'email_verified'], function () {
         Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
 
         Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');
