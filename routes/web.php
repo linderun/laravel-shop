64d2bfe15a68960156377e1df2a6c33d0b207360
Darren-Lin <554515127@qq.com>
2018-11-21 23:29:53 +0800

    用户界面 - 收藏商品列表

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,6 @@
 Route::redirect('/', '/products')->name('root');
 
 Route::get('products', 'ProductsController@index')->name('products.index');
-Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 
 Auth::routes();
 
@@ -36,5 +35,8 @@ Route::group(['middleware' => 'auth'], function () {
 
         Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
         Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
+        Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
     });
 });
+
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
