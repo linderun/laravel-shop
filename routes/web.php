d3ddaf3c006a453b9331b0439c57b69de6b43224
Darren Lin <linderun@linderun.com>
2018-11-26 21:30:38 +0800

    用户界面 - 申请退款

--- routes/web.php
+++ routes/web.php
@@ -52,6 +52,7 @@ Route::group(['middleware' => 'auth'], function () {
         Route::post('orders/{order}/received', 'OrdersController@received')->name('orders.received');
         Route::get('orders/{order}/review', 'OrdersController@review')->name('orders.review.show');
         Route::post('orders/{order}/review', 'OrdersController@sendReview')->name('orders.review.store');
+        Route::post('orders/{order}/apply_refund', 'OrdersController@applyRefund')->name('orders.apply_refund');
     });
 });
 
