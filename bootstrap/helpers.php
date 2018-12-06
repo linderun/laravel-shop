1cdd4c03b61f02ff84233f332a9a0c08487b1bba
Darren Lin <linderun@linderun.com>
2018-12-06 23:17:24 +0800

    初始化分期付款模型

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -10,6 +10,7 @@ function route_class()
     return str_replace('.', '-', Route::currentRouteName());
 }
 
+// 获取 ngrok 反向代理地址
 function ngrok_url($routeName, $parameters = [])
 {
     // 开发环境，并且配置了 NGROK_URL
@@ -19,4 +20,15 @@ function ngrok_url($routeName, $parameters = [])
     }
 
     return route($routeName, $parameters);
+}
+
+/**
+ * 实例化 BigNumber 对象
+ * @param     $number
+ * @param int $scale 默认的精度为小数点后两位
+ * @return \Moontoast\Math\BigNumber
+ */
+function big_number($number, $scale = 2)
+{
+    return new \Moontoast\Math\BigNumber($number, $scale);
 }
