a76b77029dba3809b544b91bc9e5a22cfd7b22a2
Darren Lin <linderun@linderun.com>
2018-12-06 21:20:30 +0800

    众筹结束逻辑

---
+++ app/Jobs/RefundCrowdfundingOrders.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\CrowdfundingProduct;
+use App\Models\Order;
+use App\Services\OrderService;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class RefundCrowdfundingOrders implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $crowdfunding;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(CrowdfundingProduct $crowdfunding)
+    {
+        $this->crowdfunding = $crowdfunding;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        // 如果众筹的状态不是失败则不执行退款，原则上不会发生，这里只是增加健壮性
+        if ($this->crowdfunding->status !== CrowdfundingProduct::STATUS_FAIL) {
+            return;
+        }
+        // 将定时任务中的众筹失败退款代码移到这里
+        $orderService = app(OrderService::class);
+        Order::query()
+            ->where('type', Order::TYPE_CROWDFUNDING)
+            ->whereNotNull('paid_at')
+            ->whereHas('items', function ($query) {
+                $query->where('product_id', $this->crowdfunding->product_id);
+            })
+            ->get()
+            ->each(function (Order $order) use ($orderService) {
+                $orderService->refundOrder($order);
+            });
+    }
+}
