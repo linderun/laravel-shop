167ba0388ec657cd707f7eb6879e05f8ab9e1ff6
Darren Lin <linderun@linderun.com>
2018-12-07 21:46:53 +0800

    前台分期付款详情页

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,11 +2,12 @@
 
 namespace App\Providers;
 
+use App\Models\Installment;
 use App\Models\Order;
 use App\Models\UserAddress;
+use App\Policies\InstallmentPolicy;
 use App\Policies\OrderPolicy;
 use App\Policies\UserAddressPolicy;
-use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -20,6 +21,7 @@ class AuthServiceProvider extends ServiceProvider
         'App\Model'        => 'App\Policies\ModelPolicy',
         UserAddress::class => UserAddressPolicy::class,
         Order::class       => OrderPolicy::class,
+        Installment::class => InstallmentPolicy::class,
     ];
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -60,6 +60,7 @@ Route::group(['middleware' => 'auth'], function () {
 
         Route::post('payment/{order}/installment', 'PaymentController@payByInstallment')->name('payment.installment');
         Route::get('installments', 'InstallmentsController@index')->name('installments.index');
+        Route::get('installments/{installment}', 'InstallmentsController@show')->name('installments.show');
     });
 });
 
