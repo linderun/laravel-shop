ac0ac1ac222c7bad3e5df1b701e40e4c0cac0815
Darren Lin <linderun@linderun.com>
2018-12-07 21:25:10 +0800

    创建分期付款

--- routes/web.php
+++ routes/web.php
@@ -57,6 +57,8 @@ Route::group(['middleware' => 'auth'], function () {
         Route::get('coupon_codes/{code}', 'CouponCodesController@show')->name('coupon_codes.show');
 
         Route::post('crowdfunding_orders', 'OrdersController@crowdfunding')->name('crowdfunding_orders.store');
+
+        Route::post('payment/{order}/installment', 'PaymentController@payByInstallment')->name('payment.installment');
     });
 });
 
