0d1d70918f650fb83d9ecd5edc1dbf679ccf7f08
Darren Lin <linderun@linderun.com>
2018-12-08 21:30:57 +0800

    商品添加长标题字段

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -15,7 +15,7 @@ class Product extends Model
     ];
 
     protected $fillable = [
-        'title', 'description', 'image', 'on_sale',
+        'title', 'long_title', 'description', 'image', 'on_sale',
         'rating', 'sold_count', 'review_count', 'price', 'type',
     ];
 
